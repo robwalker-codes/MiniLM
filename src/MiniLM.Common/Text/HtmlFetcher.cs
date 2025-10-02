a3c5467575eb9f8b372a8a59a27010f022f03946
Rob Walker <rob.walker@heathersoft.co.uk>
2025-10-02 16:00:36 +0100

    Fixed issue causing HTTP 403 forbidden responses

--- src/MiniLM.Common/Text/HtmlFetcher.cs
+++ src/MiniLM.Common/Text/HtmlFetcher.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,8 +12,23 @@ public sealed class HtmlFetcher
 
     public HtmlFetcher(HttpClient? httpClient = null)
     {
-        _httpClient = httpClient ?? new HttpClient();
+        var handler = new SocketsHttpHandler
+        {
+            AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate | DecompressionMethods.Brotli,
+            AllowAutoRedirect = true,
+            PooledConnectionLifetime = TimeSpan.FromMinutes(5),
+            ConnectTimeout = TimeSpan.FromSeconds(10)
+        };
+
+        _httpClient = httpClient ?? new HttpClient(handler);
+
         _httpClient.Timeout = TimeSpan.FromSeconds(10);
+
+        _httpClient.DefaultRequestHeaders.UserAgent.ParseAdd(
+            "MiniLM-TrainingBot/0.1 (+https://github.com/robwalker-codes/MiniLM; contact: me@example.com)");
+
+        _httpClient.DefaultRequestHeaders.Accept.ParseAdd("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+        _httpClient.DefaultRequestHeaders.AcceptLanguage.ParseAdd("en-GB,en;q=0.9");
     }
 
     public async Task<string?> FetchAsync(string url, CancellationToken cancellationToken)
